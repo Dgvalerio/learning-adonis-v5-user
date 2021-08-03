42ce72ccd363c195837d07fea567777133e24b1b
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-02 23:14:59 -0300

    "Adonis 5 - criando uma API com a versão oficial - Code/Drops #71"

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,7 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.group(() => {
+  Route.get('', 'UsersController.index')
+  Route.post('', 'UsersController.create')
+}).prefix('/users')
